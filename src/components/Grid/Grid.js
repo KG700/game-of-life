ee111c3b7db1194ac24c0c112086148c5824708b
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-03 22:24:11 +0100

    Cell colours update with appropriate colour depending on cell status

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,15 +1,29 @@
 import React from 'react';
 
+import classes from './Grid.module.css';
+
 const Grid = (props) => {
   const tableRows = props.cellStatus.map(row => {
     let theCells = row.map(cell => {
+      let style;
+      if (cell == 'dead') { style = classes.dead }
+      if (cell == 'lonely') { style = classes.lonely }
+      if (cell == 'alive') { style = classes.alive }
+      if (cell == 'crowded') { style = classes.crowded }
+
       return <td
-              className={cell}
+              className={style}
               >{cell}</td>
     })
     return <tr>{theCells}</tr>;
   })
-  return <table><tbody>{tableRows}</tbody></table>
+  return (
+    <table className={classes.Grid}>
+      <tbody>
+        {tableRows}
+      </tbody>
+    </table>
+  )
 }
 
 export default Grid;
