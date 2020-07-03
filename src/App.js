1d415f76b21c1fc7aea1dd3cdb8056234d3f69b9
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-03 21:01:31 +0100

    Set up App.js and set initial state to grid of dead cells

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const NUMBER_OF_ROWS = 20;
+const NUMBER_OF_COLUMNS = 30;
+
+class App extends Component {
+
+  state = {
+    gridStatus: Array(NUMBER_OF_ROWS)
+                  .fill(Array(NUMBER_OF_COLUMNS)
+                    .fill('dead')
+                  )
+  }
+
+  componentDidMount() {
+    console.log("Howdy!")
+    this.newGridHandler();
+  }
+
+  newGridHandler = () => {
+    console.log("Hello!")
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
 }
 
 export default App;
