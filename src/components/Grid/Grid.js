df770b8a18417c7dbb7b27ef518126f3b3ca726a
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-10 20:05:19 +0100

    Removed unnecessary variable from Grid.js

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -7,21 +7,12 @@ const Grid = (props) => {
   console.log('<Grid />')
   const tableRows = props.cellStatus.map(row => {
     let theCells = row.map(cell => {
-      let style;
-      if (cell === 'dead') { style = classes.dead }
-      if (cell === 'lonely') { style = classes.lonely }
-      if (cell === 'alive') { style = classes.alive }
-      if (cell === 'crowded') { style = classes.crowded }
-      if (cell === 'reviveLonely') { style = classes.reviveLonely }
-      if (cell === 'reviveAlive') { style = classes.reviveAlive }
-      if (cell === 'reviveCrowded') { style = classes.reviveCrowded }
-
       return <td>
               {cell !== 'dead' ? <Ball style={cell} /> : null}
               </td>
-    })
+    });
     return <tr>{theCells}</tr>;
-  })
+  });
 
   return (
     // <div className={classes.Container}>
