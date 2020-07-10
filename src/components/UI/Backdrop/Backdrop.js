40b88de8c2b2c5a02694ebb86ee8ce7329514884
Karla Gardiner <karla.gardiner@gmail.com>
2020-07-10 22:01:00 +0100

    Adjustment to Backdrop

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -3,35 +3,30 @@ import React from 'react';
 import classes from './Backdrop.module.css';
 
 const Backdrop = (props) => {
+  console.log('in Backdrop')
   return(
-    <>
-    {props.show
-    ?
-      <div
-        className={classes.Backdrop}
-        onMouseEnter={props.onMouseEnter}
-        onMouseLeave={props.onMouseLeave}
-      >
-        <h1>GAME OF LIFE</h1>
-        <div>
+    <div
+      className={classes.Backdrop}
+      onMouseEnter={props.onMouseEnter}
+      onMouseLeave={props.onMouseLeave}
+    >
+      <h1>GAME OF LIFE</h1>
+      <div>
+        <button
+          className={classes.Refresh}
+          onClick={props.refresh}></button>
+        {props.isRunning
+        ?
           <button
-            className={classes.Refresh}
-            onClick={props.refresh}></button>
-          {props.isRunning
-          ?
-            <button
-              className={classes.Pause}
-              onClick={props.stop}></button>
-          :
-            <button
-              className={classes.Play}
-              onClick={props.start}></button>
-          }
-        </div>
+            className={classes.Pause}
+            onClick={props.stop}></button>
+        :
+          <button
+            className={classes.Play}
+            onClick={props.start}></button>
+        }
       </div>
-    :
-      null}
-    </>
+    </div>
   )
 }
 
